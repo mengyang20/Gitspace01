d8ac66892b92732e658a1de8349fe25436c6655e
mengyang20 <1627033079@qq.com>
2021-08-07 19:33:27 +0800

    添加文件

---
+++ hellogit/src/main/java/com/atguigu/git/hellp.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @date 2021/8/7 - 18:41
+ */
+public class hellp {
+    public static void main(String[] args) {
+        System.out.println("和电脑就卡是大户");
+    }
+}
---
+++ hellogit/src/main/resources/db.project.kt
@@ -0,0 +1,5 @@
+/**
+@author shkstart
+@date 2021/8/7 - 18:24
+
+ */
